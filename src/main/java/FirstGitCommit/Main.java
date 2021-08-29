dfa239b83f591bc32b0e7651ffa5f8fb79912484
puttisainaveen <51712234+puttisainaveen@users.noreply.github.com>
2021-08-29 18:03:37 -0500

    First Git Commit
    A First Git Commit

---
+++ src/main/java/FirstGitCommit/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstGitCommit;
+
+/**
+ *
+ * @author S545269
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Section - 02 -- Sai Naveen Putti -- Success");
+        // TODO code application logic here
+    }
+    
+}
